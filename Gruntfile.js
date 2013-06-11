ddabe8c395a2119fa6c6de98f4e979146ffc2038
Marc Diethelm <marc.diethelm@namics.com>
2013-06-11 23:46:40 +0200

    sprites improvements

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,8 @@ module.exports = function(grunt) {
 				,'frontend/_terrific/_inline/css/*.less'
 			]
 			,external_css: [
-				 'frontend/_terrific/_base/css/lib/*.css'
+				 'frontend/_terrific/_base/css/sprites/*.less'
+				,'frontend/_terrific/_base/css/lib/*.css'
 				,'frontend/_terrific/_base/css/elements/*.less'
 				,'frontend/_terrific/mod-*/*.less'
 				,'frontend/_terrific/mod-*/skin/*.less'
@@ -29,12 +30,24 @@ module.exports = function(grunt) {
 				 'frontend/_static/test/*.js'
 				,'frontend/_terrific/mod-*/test/*.js'
 			]
+			,sprites: [
+				'frontend/_terrific/_base/css/sprites/'
+				//,'frontend/_terrific/mod-*/sprites/'
+			]
 		}
 		,tmp: 'frontend/_static/dist/tmp'
 		,dest: 'frontend/_static/dist'
+		,dest_sprites_css: 'frontend/_terrific/_base/css/sprites/' // technically this should go in <%=dest%>/tmp, but we want the generated classes in our base css for easy lookup.
 
 		//////////////////////////////////////////////////////////////
 
+		,glue: {
+			icons: {
+				src: '<%=sources.sprites%>'
+				//,dest: '<%=dest_sprites_css%>/00-sprites.less'
+				,options: '--css=<%=dest_sprites_css%> --img=<%=dest%> --less --url=/dist --namespace= --sprite-namespace= --recursive --crop --optipng'
+			}
+		}
 		,less_imports: {
 			inline: {
 				options: {}
@@ -96,6 +109,10 @@ module.exports = function(grunt) {
 			}
 		}
 		,watch: {
+			sprites: {
+				files: ['<%=sources.sprites%>']
+				,tasks: ['build-sprites', 'build-external-css']
+			},
 			inline_styles: {
 				files: ['<%=sources.inline_css%>']
 				,tasks: ['build-inline-css']
@@ -119,6 +136,7 @@ module.exports = function(grunt) {
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-glue');
 	grunt.loadNpmTasks('assemble-less');
 	grunt.loadNpmTasks('grunt-less-imports');
 	grunt.loadNpmTasks('grunt-contrib-concat');
@@ -127,10 +145,12 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-handlebars');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	// create pipelines                             // use actual task name (first part before colon)!
-	grunt.registerTask('build-inline-js',            ['concat:inline_scripts', 'uglify:inline']);
-	grunt.registerTask('build-external-js',          ['concat:external_scripts', 'uglify:external']);
-	grunt.registerTask('build-inline-css',           ['less_imports:inline', 'less:inline', 'cssmin:inline']);
+	// create pipelines                              // use actual task name (first part before colon)!
+	grunt.registerTask('build-sprites',              ['glue']);
+	
+	grunt.registerTask('build-inline-js',            [                 'concat:inline_scripts',   'uglify:inline']);
+	grunt.registerTask('build-external-js',          ['build-sprites', 'concat:external_scripts', 'uglify:external']);
+	grunt.registerTask('build-inline-css',           ['less_imports:inline',   'less:inline',   'cssmin:inline']);
 	grunt.registerTask('build-external-css',         ['less_imports:external', 'less:external', 'cssmin:external']);
 	grunt.registerTask('build-module-tests',         ['concat:module_tests']);
 
