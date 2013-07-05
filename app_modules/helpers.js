dda405e7e4bf4d220f69b039b6c06c6c9cecd60d
Marc Diethelm <marc.diethelm@namics.com>
2013-07-05 13:02:02 +0200

    minimally enhanced error handling

--- app_modules/helpers.js
+++ app_modules/helpers.js
@@ -13,10 +13,11 @@ module.exports = function(app) {
 			,originalMessage = originalError && originalError.message || ''
 		;
 		message = message || '';
-		err += message + ' Reason: ' + originalMessage;
+		err += message + ' \nReason: ' + originalMessage;
 		return {
 			c: err
 			,web: '\n'+err
+			,code: originalError.code
 		};
 	};
 
