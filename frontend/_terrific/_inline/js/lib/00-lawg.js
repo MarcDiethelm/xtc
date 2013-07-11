cfcf4a40958d693c047d4c68b11d545240203a03
Marc Diethelm <marc.diethelm@namics.com>
2013-07-11 13:46:54 +0200

    update lawg.js

--- frontend/_terrific/_inline/js/lib/00-lawg.js
+++ frontend/_terrific/_inline/js/lib/00-lawg.js
@@ -1,21 +1,23 @@
 /**
- * lawg.js
- *
- * A logging and debugging helper for the browser
+ * Global development and debugging helper
  * Author: Marc Diethelm
- *
- * see README-lawg.md
  */
 
-(function() {
+;(function() {
 
 	var  w = window
 		,c
+		,$ = w.jQuery || w.Zepto || null
 		,isElement
 		,isjQuery
 		,isArray
+		,fn = 'function'
+		,cMethods = ['log', 'info', 'debug', 'warn', 'error', 'dir', 'table']
+		,method
+		,i
+		,len
 	;
-	
+
 	function concatArgs(args, nestedCall) {
 		var aStr = []
 			,i
@@ -53,11 +55,11 @@
 		var htmlId = ''
 		    ,htmlClass = ''
 		;
-		elem.id && (htmlId = '#' + elem.id); 
+		elem.id && (htmlId = '#' + elem.id);
 		elem.className && (htmlClass = '.' + elem.className.replace(/\s/g, '.')); // replace any white space with a '.'
 		return elem.tagName + htmlId + htmlClass;
 	}
-	
+
 	function jQueryToString($obj) {
 		var i
 			,stringArray = []
@@ -65,10 +67,10 @@
 		for (i=0; i<$obj.length; i++) {
 			stringArray.push( elementToString($obj[i]) );
 		}
-		
+
 		return '$('+ stringArray.join(', ') +')';
 	}
-	
+
 	//Resulting function returns true if param is a DOM element
 	if (typeof HTMLElement == 'function') {
 		isElement = function(obj) { return obj instanceof HTMLElement }
@@ -76,28 +78,27 @@
 	else {
 		isElement = function(obj) { return typeof obj == 'object' && obj.nodeType === 1 && typeof obj.nodeName==='string' }
 	}
-	
+
 	// Resulting function returns true if param is a jQuery object
 	try {
-		$() instanceof jQuery; // abusing try/catch for flow control. Old crappy browsers (I'm lookin at you IE) will throw.
-		isjQuery = function(obj) { return obj instanceof jQuery }
+		$() instanceof $; // abusing try/catch for flow control. Old crappy browsers (I'm lookin at you IE) will throw.
+		isjQuery = function(obj) { return obj instanceof $ }
 	} catch (e) {
 		isjQuery = function(obj) { return typeof obj == 'object' && 'jquery' in obj && typeof obj.jquery == 'string' }
 	}
 
 	isArray = Array.isArray || function(obj) {
-		return toString.call(obj) == '[object Array]';
+		return Object.prototype.toString.call(obj) == '[object Array]';
 	};
-	
+
 	if (w.console) {
 		c = console;
 		if ('dir' in c && 'apply' in c.dir) { // create global shortcuts
-			typeof c.log   === 'function' && ( w.log   = function() { c.log.apply(c, arguments) } );
-			typeof c.info  === 'function' && ( w.info  = function() { c.info.apply(c, arguments) } );
-			typeof c.debug === 'function' && ( w.debug = function() { c.debug.apply(c, arguments) } );
-			typeof c.error === 'function' && ( w.error = function() { c.error.apply(c, arguments) } );
-			typeof c.dir   === 'function' && ( w.dir   = function() { c.dir.apply(c, arguments) } );
-			typeof c.table === 'function' && ( w.table = function() { c.table.apply(c, arguments) } );
+			for (i=0, len = cMethods.length; i<len && (method = cMethods[i]); i++) {
+				typeof c[method] === fn && ( function(method) { // create a new scope to preserve the value of method
+					w[method] = function() { c[method].apply(c, arguments) }
+				} )(method);
+			}
 		}
 		else { // IE: we have console.log but it just accepts one param. let's fix that! :)
 			w.log = function() {
@@ -110,22 +111,23 @@
 	else {
 		w.log = function() { alert(concatArgs(arguments)) };
 	}
-	
+
 	w._alert = function() { alert(concatArgs(arguments)) };
 
 	/**
 	 * A tiny jQuery plugin adding logging to any jQuery object
 	 * @param clear Boolean Clear the console before logging?
 	 */
-	
-	w.jQuery && (jQuery.fn.log = function(clear) {
+
+	$ && ($.fn.log = function(clear) {
 		clear && c.clear.call(c);
 		c.log.call(c, this);
 		return this;
 	});
-	
-	w.jQuery && (jQuery.fn.alert = function() {
+
+	$ && ($.fn.alert = function() {
 		w._alert(concatArgs(this));
 	});
+
 	
 })();
