2ae089b2e40b8d93ce406a333948373d7930c1f5
Marc Diethelm <marc@web5.me>
2013-10-26 13:48:52 +0200

    Shorten renderModule test descriptions.

--- test/terrific-modules/terrific.js
+++ test/terrific-modules/terrific.js
@@ -9,7 +9,7 @@ var assert = require('assert');
 var grunt = require('grunt');
 var terrific, config;
 
-describe('terrific::renderModule', function() {
+describe('renderModule', function() {
 
 	before(function() {
 		//this.server = app.listen(3000);
@@ -38,42 +38,42 @@ describe('terrific::renderModule', function() {
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-read-error.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with alternate template', function() {
+	it('should render a module with alternate template', function() {
 		var actual = terrific.renderModule({}, { name: 'basic', template: 'alternate' });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-alternate.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with skins', function() {
+	it('should render a module with skins', function() {
 		var actual = terrific.renderModule({}, { name: 'basic', skins: ['zebra', 'crocodile'] });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-skins.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with a custom tag', function() {
+	it('should render a module with a custom tag', function() {
 		var actual = terrific.renderModule({}, { name: 'basic', tag: 'article' });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-tag.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with an id attribute', function() {
+	it('should render a module with an id attribute', function() {
 		var actual = terrific.renderModule({}, { name: 'basic', id: 'unique' });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-id.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with specified attributes, incl. data-connectors', function() {
+	it('should render a module with specified attributes, incl. data-connectors', function() {
 		var actual = terrific.renderModule({}, { name: 'basic', id: 'unique', attributes: [{key:'data-connectors', value:'stats, filter'}, {key:'lang', value:'de-CH'}, {key:'data-1', value: "{'key': 'val'}"}] });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-attributes.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with HTML classes', function() {
+	it('should render a module with HTML classes', function() {
 		var actual = terrific.renderModule({}, { name: 'basic', htmlClasses: 'vis-hidden form' });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-html-classes.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with injected data merged with context', function() {
+	it('should render a module with injected data merged with context', function() {
 		var actual = terrific.renderModule({ a: 'a-ctx', b: 'b-ctx' }, { name: 'basic', template: 'data', data: {a: 'a-mod', c: 'c-mod'} });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-data.html');
 		assert.equal(actual, expected);
 	});
-	it('should render a basic module, with a nested module', function() {
+	it('should render a module with a nested module', function() {
 		var actual = terrific.renderModule({ fee: 100, foo: 'bar' }, { name: 'basic', template: 'nested', data: {fee: 'fi', fo: 'fum'} });
 		var expected = grunt.file.read('test/terrific-modules/expected/basic/basic-nested.html');
 		assert.equal(actual, expected);
