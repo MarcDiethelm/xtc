b659ed70438151d30df36ec8aed5ccfba7e27499
Marc Diethelm <marc@web5.me>
2014-03-11 14:32:56 +0100

    Fix version output

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // log node version, xtc version and server mode
 process.env.NODE_ENV != 'test' && console.log('node %s – xtc v%s – server in %s mode',
-	process.version, require('./package.json').xtcVersion, process.env.NODE_ENV
+	process.version, require('./package.json').version, process.env.NODE_ENV
 );
 
 
