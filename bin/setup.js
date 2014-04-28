f949f680d672362c640d5bc11a8808551355371f
Marc Diethelm <marc@web5.me>
2014-04-28 11:58:17 +0200

    Yoda conditions and store process.cwd()

--- bin/setup.js
+++ bin/setup.js
@@ -4,15 +4,14 @@
 // Else Yeoman won't find it.
 
 var path = require('path')
-	fs = require('fs')
+	,fs = require('fs')
+	,cwd = process.cwd
 ;
-
-// process.cwd() == __dirname
 
 if ('install' === process.env.npm_lifecycle_event) {
 
 	try {
-		fs.symlinkSync(path.join(process.cwd(), '/node_modules/generator-xtc'), path.join(process.cwd(), '../generator-xtc'), 'dir');
+		fs.symlinkSync(path.join(cwd, '/node_modules/generator-xtc'), path.join(cwd, '../generator-xtc'), 'dir');
 		console.log('symlink: generator-xtc into node_modules\n')
 	} catch (e) {
 		if ('EEXIST' === e.code) {
@@ -33,10 +32,10 @@ if ('install' === process.env.npm_lifecycle_event) {
 else if ('uninstall' === process.env.npm_lifecycle_event) {
 
 	try {
-		fs.unlinkSync(path.join(process.cwd(), '../generator-xtc'), 'dir');
+		fs.unlinkSync(path.join(cwd, '../generator-xtc'), 'dir');
 		console.log('symlink: removed generator-xtc from node_modules\n')
 	} catch (e) {
-		if (e.code !== 'ENOENT') {
+		if ('ENOENT' !== e.code) {
 			console.info('symlink: Unable to remove generator-xtc from node_modules\n');
 		}
 		throw e;
